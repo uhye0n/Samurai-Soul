6a0d9123374db422e0c4a79a634e022295a15977
uhye0n <prisoner1044@gmail.com>
2024-12-22 14:33:38 +0900

    2024-12-22

--- Assets/1. Scripts/Managers/GameOverManager.cs
+++ Assets/1. Scripts/Managers/GameOverManager.cs
@@ -6,31 +6,18 @@ using TMPro;
 public class GameOverManager : MonoBehaviour
 {
     [Header("UI Elements")]
-    [SerializeField] private TMP_Text gameOverText;
     [SerializeField] private Button retryButton;
     [SerializeField] private Button menuButton;
-    [SerializeField] private float textPulseSpeed = 1f;
 
     private void Start()
     {
         InitializeUI();
-        StartCoroutine(PulseTextEffect());
     }
 
     private void InitializeUI()
     {
         retryButton.onClick.AddListener(RetryGame);
         menuButton.onClick.AddListener(ReturnToMenu);
-    }
-
-    private System.Collections.IEnumerator PulseTextEffect()
-    {
-        while (true)
-        {
-            float pulse = (Mathf.Sin(Time.unscaledTime * textPulseSpeed) + 1) / 2;
-            gameOverText.color = new Color(1f, 0f, 0f, 0.5f + (pulse * 0.5f));
-            yield return null;
-        }
     }
 
     private void RetryGame()
